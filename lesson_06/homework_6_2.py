49bf6803efe79645cc5b33d1779c4493af05381f
o.bogachenko <abogachenko@gmail.com>
2024-09-01 18:41:58 +0300

    add homework_6_2

---
+++ lesson_06/homework_6_2.py
@@ -0,0 +1,8 @@
+var_word = input("Enter a word with letter H: ").strip()
+var_word_upper = var_word.upper()
+
+if 'H' in var_word_upper:
+    print(f"You enter a {var_word} with letter H")
+else:
+    print(f"You enter a {var_word} without letter H")
+
