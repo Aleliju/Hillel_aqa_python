d18fbd0bf7d9c20dbe54519135e482130ff22e3f
o.bogachenko <abogachenko@gmail.com>
2024-09-01 19:39:08 +0300

    add while homework_6_2

--- lesson_06/homework_6_2.py
+++ lesson_06/homework_6_2.py
@@ -1,8 +1,10 @@
-var_word = input("Enter a word with letter H: ").strip()
-var_word_upper = var_word.upper()
+while True:
+    var_word = input("Enter a word with letter H: ").strip()
+    var_word_upper = var_word.upper()
 
-if 'H' in var_word_upper:
-    print(f"You enter a {var_word} with letter H")
-else:
-    print(f"You enter a {var_word} without letter H")
+    if 'H' in var_word_upper:
+        print(f"You enter a {var_word} with letter H")
+        break
+    else:
+        print(f"You enter a {var_word} without letter H")
 
